9ccd24c6b26d31b45c1ba2417360b91c3b523a71
Stepan <bla-stepan@mail.ru>
2022-03-11 08:41:50 +0300

    изменен конструктор - добавлен принимаемый параметр курс

--- src/Student.java
+++ src/Student.java
@@ -5,11 +5,11 @@ public class Student {
     int course;
     int grupp;
 
-    public Student(String name, String sername, String fakulty) {
+    public Student(String name, String sername, String fakulty, int course) {
         this.name = name;
         this.sername = sername;
         this.fakulty = fakulty;
         int grupp = 1;
-        int course = 2;
+        this.course = course;
     }
 }
