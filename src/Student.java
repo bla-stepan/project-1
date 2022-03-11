a88c1021140b132c771932b1417a1a8a30b9a60e
Stepan <bla-stepan@mail.ru>
2022-03-11 08:12:59 +0300

    добавлен конструктор, принимающий параметры имя, фамилия и факультет и по умолчанию устанавливающий 1 курс и 1 группу

--- src/Student.java
+++ src/Student.java
@@ -4,4 +4,12 @@ public class Student {
     String fakulty;
     int course;
     int grupp;
+
+    public Student(String name, String sername, String fakulty) {
+        this.name = name;
+        this.sername = sername;
+        this.fakulty = fakulty;
+        int grupp = 1;
+        int course = 1;
+    }
 }
